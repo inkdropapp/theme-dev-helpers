8987dcab4c4a1ba0396575b9cc4a3aed563cad29
Takuya Matsuyama <hi@craftz.dog>
2024-10-21 16:37:31 +0900

    fix(project): vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const baseProjectPath = process.env.BASE_PROJECT_PATH || fileURLToPath(new URL('../..', import.meta.url))
+const baseProjectPath = process.env.BASE_PROJECT_PATH || fileURLToPath(new URL('../../..', import.meta.url))
 console.log('Base project path:', baseProjectPath)
 
 // https://vitejs.dev/config/
